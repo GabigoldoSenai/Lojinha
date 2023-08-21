9a1a1210e7858eade5eed305dac6e6909277a0ef
Gabigol dos croche <Ohlekaa@gmail.com>
2023-08-21 20:07:38 -0300

    Bugs corrigidos e sistema de espuma, feltro, olhos e tempo concluídas

--- calculos.js
+++ calculos.js
@@ -6,16 +6,17 @@ switch(menu){
     //>>> PRIMEIRO CASO: CÁLCULO DE PREÇO
     case 1:
         /* Materiais base (preços arredondados):
-        Lã normal(40g): R$5,00 || Preço/g : R$0,13
-        Espuma (1kg) : R$39,00 || Preço/g : R$0,04
-        Feltro (1,4m²) : R$20,00 || Preço/cm²: 0,0142
-        Olhos amigurumi : R$5,00 || Preço/uni : 0,5
+        Lã normal(40g): R$5.00 || Preço/g : R$0.13
+        Espuma (1kg) : R$39.00 || Preço/g : R$0.04
+        Feltro (1.4m²) : R$20.00 || Preço/cm²: 0.0142
+        Olhos amigurumi : R$5.00 || Preço/uni : 0.5
 
-        Tempo : R$3,00/h
+        Tempo : R$3.00/h
         */
         
         let la, valor_la = 0 , tipo_la, tamanho_la, espuma  , feltro , olhos , custo , tempo , preco , lucro_bruto , investimento , lucro_liquido 
-
+        
+        /*
         //---- Valores para lã
         let i = Number(prompt("Quantos tipos de lã você utilizou?"))
         //
@@ -68,31 +69,81 @@ switch(menu){
                 n --
             }
         }
-        //
+        */
         //---- VALORES DE ESPUMA
-        espuma = Number(prompt("Digite o peso total de espuma (em g) usado: "))
-        if(isNaN(espuma) || espuma < 0){
-            for(; isNaN(espuma) || espuma < 0;){
+        espuma = prompt(`Digite o peso total de espuma (em g) usado:\nSe não usou, digite "n" `)
+        debugger
+        if(espuma <= 0){
+            for(; espuma <= 0;){
                 alert("ERRO - Caracteres inválidos")
-                espuma = Number(prompt("Digite o peso total de espuma (em g) usado: "))
+                espuma = prompt(`Digite o peso total de espuma (em g) usado:\nSe não usou, digite "n" `)
             }
+        }else if(espuma > 0){
         }else{
-        espuma = espuma * 0.4
+            for(; espuma <= 0 || espuma !== "n" && "N";){
+                alert("ERRO - Caracteres inválidos")
+                espuma = prompt(`Digite o peso total de espuma (em g) usado:\nSe não usou, digite "n" `)
+            }
         }
+        if(espuma == "n" || espuma == "N"){
+        alert("Você não usou espuma")
+        espuma = 0
+        }
+        espuma = espuma * 0.4
 
         //---- VALORES DE FELTRO
-        feltro = Number(prompt("Digite o tamanho total de feltro (em cm²) usado: "))
-        feltro = feltro * 0.142
+        feltro = prompt(`Digite o tamanho total de feltro (em cm²) usado:\nSe não usou, digite "n" `)
+        debugger
+        if(feltro <= 0){
+            for(; feltro <= 0;){
+                alert("ERRO - Caracteres inválidos")
+                feltro = prompt(`Digite o tamanho total de feltro (em cm²) usado:\nSe não usou, digite "n" `)
+            }
+        }else if(feltro > 0){
+        }else{
+            for(; feltro <= 0 || feltro !== "n" && "N";){
+                alert("ERRO - Caracteres inválidos")
+                feltro = prompt(`Digite o tamanho total de feltro (em cm²) usado:\nSe não usou, digite "n" `)
+            }
+        }
+        if(feltro == "n" || feltro == "N"){
+        alert("Você não usou feltro")
+        feltro = 0
+        }
+        feltro = feltro * 0.0142
 
         //---- VALORES DE OLHOS 
-        olhos = Number(prompt("Digite o total de olhos de amigurumi usados: "))
+        olhos = prompt(`Digite o total de olhos usado:\nSe não usou, digite "n" `)
+        debugger
+        if(olhos <= 0){
+            for(; olhos <= 0;){
+                alert("ERRO - Caracteres inválidos")
+                olhos = prompt(`Digite o total de olhos usado:\nSe não usou, digite "n" `)
+            }
+        }else if(olhos > 0){
+        }else{
+            for(; olhos <= 0 || olhos !== "n" && "N";){
+                alert("ERRO - Caracteres inválidos")
+                olhos = prompt(`Digite o total de olhos usado:\nSe não usou, digite "n" `)
+            }
+        }
+        if(olhos == "n" || olhos == "N"){
+        alert("Você não usou olhos")
+        olhos = 0
+        }
         olhos = olhos * 0.5
 
         //---- CÁLCULO DE PREÇO, CUSTO, ETC..
         custo = la + espuma + feltro + olhos
 
         tempo = Number(prompt("Insira o tempo que você levou até terminar (em h): "))
-        // PADRÃO: R$3,00/h (metade do salario minimo)
+        if(tempo <= 0){
+            for(; tempo <= 0;){
+            alert("ERRO - Valores inválidos")
+            tempo = Number(prompt("Insira o tempo que você levou até terminar (em h): "))
+            }
+        }
+        // PADRÃO: R$3,00/h (metade de um salario minimo)
         tempo = tempo * 3
 
         preco = Math.ceil(custo) + tempo
@@ -103,11 +154,7 @@ switch(menu){
 
         document.write(`Custo de produção: R$${custo.toFixed(2)} <br> Custo de produção +35% : R$${(custo * 1.35).toFixed(2)} <br> Sugestão de preço: R$${Math.ceil(preco).toFixed(2)}<br><br> ||Lucro bruto: R$${lucro_bruto.toFixed(2)} | Para investir/guardar: R$${investimento.toFixed(2)} | Lucro líquido: R$${lucro_liquido.toFixed(2)}`)
         break;
-    
-        alert("ERROR")
-        location.reload()
-        break;
-        
+       
         // >>>> NÃO É NENHUMA DAS OPÇÕES
         default: 
         alert("ERRO - Valor inválido")
