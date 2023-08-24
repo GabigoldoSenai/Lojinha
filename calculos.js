4437f50439345979ac1105ad62f254a1d14e2826
Gabigol dos croche <Ohlekaa@gmail.com>
2023-08-24 00:21:00 -0300

    For's transformados em while's e outras gambiarras desfeitas

--- calculos.js
+++ calculos.js
@@ -1,183 +1,188 @@
 let menu
 
-menu = Number(prompt("Que tipo de cálculo você quer fazer? \n 1) Cálculo de preço"))
+debugger
 
-switch(menu){
-    //>>> PRIMEIRO CASO: CÁLCULO DE PREÇO
-    case 1:
-        /* Materiais base (preços arredondados):
-        Lã normal(40g): R$5.00 || Preço/g : R$0.13
-        Espuma (1kg) : R$39.00 || Preço/g : R$0.04
-        Feltro (1.4m²) : R$20.00 || Preço/cm²: 0.0142
-        Olhos amigurumi : R$5.00 || Preço/uni : 0.5
+const btn_start = document.getElementById("btn_start")
 
-        Tempo : R$3.00/h
-        */
-        
-        let la, valor_la = 0 , tipo_la, tamanho_la, espuma  , feltro , olhos , custo , tempo , preco , lucro_bruto , investimento , lucro_liquido 
-        
-        
-        //---- Valores para lã
-        let i = Number(prompt("Quantos tipos de lã você utilizou?"))
-        //
-        if((i > 3 || i <= 0 ) || isNaN(i)){
-            for(; (i > 3 || i <= 0 ) || isNaN(i);){
-                    alert("ERRO - Caracteres inválidos")
-                    i = Number(prompt("Quantos tipos de lã você utilizou?"))
+btn_start.addEventListener("click", fn_start)
+
+function fn_start(){
+    menu = Number(prompt("Que tipo de cálculo você quer fazer? \n 1) Cálculo de preço\n 2) Catálogo de preços base"))
+
+    switch(menu){
+
+        //>>> PRIMEIRO CASO: CÁLCULO DE PREÇO
+        case 1:
+            /*
+            let continuar = confirm("Bem-vinda ao cálculo de preço!\nContinuar?")
+            if(continuar == false){
+                location.reload()
             }
-        }
-        //
-        for(let n = 0; i > n ; n ++){
-            tipo_la = prompt("Qual(is) são os tipos de lã usadas?\n opções: fina, normal, grossa")
-            tipo_la = tipo_la.toUpperCase()
 
+            /* Materiais base (preços arredondados):
+            Lã normal(40g): R$5.00 || Preço/g : R$0.13
+            Espuma (1kg) : R$39.00 || Preço/g : R$0.04
+            Feltro (1.4m²) : R$20.00 || Preço/cm²: 0.0142
+            Olhos amigurumi : R$5.00 || Preço/uni : 0.5
+
+            Tempo : R$3.00/h
+            */
             debugger
+            let la = {quantidade : 0, peso: 0, preco_total: 0, tipo: 0 }, espuma  , feltro , olhos , preco = {lucro_bruto: 0 , investimento: 0  , lucro_liquido: 0  , custo: 0 , tempo: 0}
             
-            if(tipo_la == "FINA" || tipo_la == "NORMAL" || tipo_la == "GROSSA"){
-                switch(tipo_la){
-                    case "FINA":
-                        la = Number(prompt("Digite o peso total de lã (em g) usado: "))
-                        la = la * 0.08
-                    break;
-                    case "NORMAL":
-                        la = Number(prompt("Digite o peso total de lã (em g) usado: "))
-                        la = la * 0.13
-                    break;
-                    case "GROSSA":
-                        la = Number(prompt("Digite o peso total de lã (em g) usado: "))
-                        la = la * 0.24    
-                    break;
-                    }
+            
+            //---- Valores para lã
+            la.quantidade = Number(prompt("Quantos tipos de lã você utilizou?"))
+            //
 
-                    if(la <= 0 || isNaN(la)){
-                        for(; la <= 0 || (isNaN(la));){
+            while(la.quantidade > 3 || la.quantidade <= 0  || isNaN(la.quantidade)){
+                alert("ERRO - Caracteres inválidos")
+                la.quantidade= Number(prompt("Quantos tipos de lã você utilizou?"))
+            }
+
+            //
+            for(let n = 0; la.quantidade > n ; n ++){
+                la.tipo = prompt("Qual(is) são os tipos de lã usadas?\n opções: fina, normal, grossa")
+                la.tipo = la.tipo.toUpperCase()
+
+                debugger
+                
+                if(la.tipo == "FINA" || la.tipo == "NORMAL" || la.tipo == "GROSSA"){
+                    switch(la.tipo){
+                        case "FINA":
+                            la.preco= Number(prompt("Digite o peso total de lã (em g) usado: "))
+                            la.preco= la.preco* 0.08
+                        break;
+                        case "NORMAL":
+                            la.preco= Number(prompt("Digite o peso total de lã (em g) usado: "))
+                            la.preco= la.preco* 0.13
+                        break;
+                        case "GROSSA":
+                            la.preco= Number(prompt("Digite o peso total de lã (em g) usado: "))
+                            la.preco= la.preco* 0.24    
+                        break;
+                        }
+
+                    while(la.preco<= 0 || (isNaN(la.preco))) {
                             alert("ERRO - Caracteres inválidos")
-                            la = Number(prompt("Digite o peso total de lã (em g) usado: "))
+                            la.preco= Number(prompt("Digite o peso total de lã (em g) usado: "))
+                        if(la.tipo == "FINA"){
+                            la.preco= la.preco* 0.08
+                        }else if(la.tipo == "NORMAL"){
+                            la.preco= la.preco* 0.13
+                        }else{
+                            la.preco= la.preco* 0.24
                         }
-                            if(tipo_la == "FINA"){
-                                la = la * 0.08
-                            }else if(tipo_la == "NORMAL"){
-                                la = la * 0.13
-                            }else{
-                                la = la * 0.24
-                            }
-                        }
-
-                    valor_la = valor_la + la
+                    }
+                    la.preco_total = la.preco_total + la.preco
+                }else{
+                    alert("ERRO - Tipo inválido")
+                    n --
+                }
+            }
+            
+            //---- VALORES DE ESPUMA
+            espuma = prompt(`Digite o peso total de espuma (em g) usado:\nSe não usou, digite "n" `)
+            debugger
+            if(espuma <= 0){
+                while( espuma <= 0){
+                    alert("ERRO - Caracteres inválidos")
+                    espuma = prompt(`Digite o peso total de espuma (em g) usado:\nSe não usou, digite "n" `)
+                }
+            }else if(espuma > 0){
             }else{
-                alert("ERRO - Tipo inválido")
-                n --
+                while( espuma <= 0 || espuma !== "n" && "N"){
+                    alert("ERRO - Caracteres inválidos")
+                    espuma = prompt(`Digite o peso total de espuma (em g) usado:\nSe não usou, digite "n" `)
+                }
             }
-        }
+            if(espuma == "n" || espuma == "N"){
+            alert("Você não usou espuma")
+            espuma = 0
+            }
+            espuma = espuma * 0.4
+
+            //---- VALORES DE FELTRO
+            feltro = prompt(`Digite o tamanho total de feltro (em cm²) usado:\nSe não usou, digite "n" `)
+            debugger
+            if(feltro <= 0){
+                while( feltro <= 0){
+                    alert("ERRO - Caracteres inválidos")
+                    feltro = prompt(`Digite o tamanho total de feltro (em cm²) usado:\nSe não usou, digite "n" `)
+                }
+            }else if(feltro > 0){
+            }else{
+                while( feltro <= 0 || feltro !== "n" && "N"){
+                    alert("ERRO - Caracteres inválidos")
+                    feltro = prompt(`Digite o tamanho total de feltro (em cm²) usado:\nSe não usou, digite "n" `)
+                }
+            }
+            if(feltro == "n" || feltro == "N"){
+            alert("Você não usou feltro")
+            feltro = 0
+            }
+            feltro = feltro * 0.0142
+
+            //---- VALORES DE OLHOS 
+            olhos = prompt(`Digite o total de olhos usado:\nSe não usou, digite "n" `)
+            debugger
+            if(olhos <= 0){
+                while( olhos <= 0){
+                    alert("ERRO - Caracteres inválidos")
+                    olhos = prompt(`Digite o total de olhos usado:\nSe não usou, digite "n" `)
+                }
+            }else if(olhos > 0){
+            }else{
+                while( olhos <= 0 || olhos !== "n" && "N"){
+                    alert("ERRO - Caracteres inválidos")
+                    olhos = prompt(`Digite o total de olhos usado:\nSe não usou, digite "n" `)
+                }
+            }
+            if(olhos == "n" || olhos == "N"){
+            alert("Você não usou olhos")
+            olhos = 0
+            }
+            olhos = olhos * 0.5
+
+            //---- CÁLCULO DE PREÇO, CUSTO, ETC..
+            preco.custo = la.preco_total + espuma + feltro + olhos
+
+            preco.tempo = Number(prompt("Insira o tempo que você levou até terminar (em h): "))
+            if(preco.tempo <= 0){
+                while( preco.tempo <= 0){
+                alert("ERRO - Valores inválidos")
+                preco.tempo = Number(prompt("Insira o tempo que você levou até terminar (em h): "))
+                }
+            }
+            // PADRÃO: R$3,00/h (metade de um salario minimo)
+            debugger
+            preco.tempo = preco.tempo * 6
+
+            preco = Math.ceil(preco.custo) + preco.tempo
+            preco.lucro_bruto = preco - preco.custo
+            preco.investimento = preco.lucro_bruto * 0.3
+            preco.lucro_liquido = preco.lucro_bruto - preco.investimento
+            
+
+            document.write(`Custo de produção: R$${preco.custo.toFixed(2)} <br> Custo de produção +35% : R$${(preco.custo * 1.35).toFixed(2)} <br> Sugestão de preço: R$${Math.ceil(preco).toFixed(2)}<br><br> ||Lucro bruto: R$${preco.lucro_bruto.toFixed(2)} | Para investir/guardar: R$${preco.investimento.toFixed(2)} | Lucro líquido: R$${preco.lucro_liquido.toFixed(2)}`)
+            break;
         
-        //---- VALORES DE ESPUMA
-        espuma = prompt(`Digite o peso total de espuma (em g) usado:\nSe não usou, digite "n" `)
-        debugger
-        if(espuma <= 0){
-            for(; espuma <= 0;){
-                alert("ERRO - Caracteres inválidos")
-                espuma = prompt(`Digite o peso total de espuma (em g) usado:\nSe não usou, digite "n" `)
-            }
-        }else if(espuma > 0){
-        }else{
-            for(; espuma <= 0 || espuma !== "n" && "N";){
-                alert("ERRO - Caracteres inválidos")
-                espuma = prompt(`Digite o peso total de espuma (em g) usado:\nSe não usou, digite "n" `)
-            }
-        }
-        if(espuma == "n" || espuma == "N"){
-        alert("Você não usou espuma")
-        espuma = 0
-        }
-        espuma = espuma * 0.4
+            // >>>> NÃO É NENHUMA DAS OPÇÕES
+            case 2:
+                let menu_2
 
-        //---- VALORES DE FELTRO
-        feltro = prompt(`Digite o tamanho total de feltro (em cm²) usado:\nSe não usou, digite "n" `)
-        debugger
-        if(feltro <= 0){
-            for(; feltro <= 0;){
-                alert("ERRO - Caracteres inválidos")
-                feltro = prompt(`Digite o tamanho total de feltro (em cm²) usado:\nSe não usou, digite "n" `)
-            }
-        }else if(feltro > 0){
-        }else{
-            for(; feltro <= 0 || feltro !== "n" && "N";){
-                alert("ERRO - Caracteres inválidos")
-                feltro = prompt(`Digite o tamanho total de feltro (em cm²) usado:\nSe não usou, digite "n" `)
-            }
-        }
-        if(feltro == "n" || feltro == "N"){
-        alert("Você não usou feltro")
-        feltro = 0
-        }
-        feltro = feltro * 0.0142
+                menu_2 = Number(prompt("Os modelos padrões disponíveis são:\n 1) Boneco [M]"))
+                
+                switch(menu_2){
+                    case 1:
+                        document.write("Preço base:<br>mínimo:R$24.00   meio-termo= R$32.00    máximo:R$39.00<br> Média: R$32.00<br>preco.custo base + 35% = R$10.98<br> Lucro líquido = Lucro líquido: R$11.81 || Para investir/guardar: R$5.06")
+                        break;
+                    default:
+                        alert("Opção não")
 
-        //---- VALORES DE OLHOS 
-        olhos = prompt(`Digite o total de olhos usado:\nSe não usou, digite "n" `)
-        debugger
-        if(olhos <= 0){
-            for(; olhos <= 0;){
-                alert("ERRO - Caracteres inválidos")
-                olhos = prompt(`Digite o total de olhos usado:\nSe não usou, digite "n" `)
-            }
-        }else if(olhos > 0){
-        }else{
-            for(; olhos <= 0 || olhos !== "n" && "N";){
-                alert("ERRO - Caracteres inválidos")
-                olhos = prompt(`Digite o total de olhos usado:\nSe não usou, digite "n" `)
-            }
-        }
-        if(olhos == "n" || olhos == "N"){
-        alert("Você não usou olhos")
-        olhos = 0
-        }
-        olhos = olhos * 0.5
+                }          
 
-        //---- CÁLCULO DE PREÇO, CUSTO, ETC..
-        custo = la + espuma + feltro + olhos
-
-        tempo = Number(prompt("Insira o tempo que você levou até terminar (em h): "))
-        if(tempo <= 0){
-            for(; tempo <= 0;){
-            alert("ERRO - Valores inválidos")
-            tempo = Number(prompt("Insira o tempo que você levou até terminar (em h): "))
-            }
-        }
-        // PADRÃO: R$3,00/h (metade de um salario minimo)
-        tempo = tempo * 3
-
-        preco = Math.ceil(custo) + tempo
-        lucro_bruto = preco - custo
-        investimento = lucro_bruto * 0.3
-        lucro_liquido = lucro_bruto - investimento
-        
-
-        document.write(`Custo de produção: R$${custo.toFixed(2)} <br> Custo de produção +35% : R$${(custo * 1.35).toFixed(2)} <br> Sugestão de preço: R$${Math.ceil(preco).toFixed(2)}<br><br> ||Lucro bruto: R$${lucro_bruto.toFixed(2)} | Para investir/guardar: R$${investimento.toFixed(2)} | Lucro líquido: R$${lucro_liquido.toFixed(2)}`)
-        break;
-       
-        // >>>> NÃO É NENHUMA DAS OPÇÕES
-        default: 
-        alert("ERRO - Valor inválido")
+            default: 
+            alert("ERRO - Valor inválido")
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
